1db2629bbc448f538b297dbec84ba2fee9053999
jhchoi <a01046765331@gmail.com>
2026-04-22 15:55:56 +0900

    feat: add node-exporter for server metrics

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,6 +100,24 @@ resource "aws_security_group" "fitlio_sg" {
     description = "Fitlio API"
   }
 
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Grafana"
+  }
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Prometheus"
+  }
+
+  
   egress {
     from_port   = 0
     to_port     = 0
